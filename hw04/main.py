8b9b491269747b5bc6f19edbe43eb08a0b5b5dee
evan0312 <37677652+evan0312@users.noreply.github.com>
2018-11-29 17:58:57 +0800

    Update main.py

--- hw04/main.py
+++ hw04/main.py
@@ -2,67 +2,37 @@ import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        html = '''
-        <html>
-        <head>
-	    <title>九九乘法表</title>
-	    <style>
-	    table{
-            width:600px;
-		    border-collapse:collapse;
-		    }
-	    table td{
-            text-align:center;
-		    border:#145b7d 1px solid
-		    }
-	    </style>
-	    </head>
-        <body>
-        <table>
-        '''
-
-        for i in range(1,10):
-            html += '<tr>'
-            for j in range(1, i+1):
-                html += '<td>%d*%d=%d</d>'%(i,j,i*j)
-            html += '</tr>'
-        
-        html += '''
-        </table>
-        </body>
-        </html>
-        '''
-        self.write(html)
-
-class SecondHandler(tornado.web.RequestHandler):
-    def get(self,m):
-        m = int(m)
+    def get(self, m):
+        if m == None:
+            m = 9;
+        else:
+            m = int(m);
+        num = int(self.get_argument('n',10))
+        if 0< num <10:
+            m = num
         html = '''
         <html>
         <head>
 	    <title>乘法表</title>
 	    <style>
 	    table{
-		    width:600px;
-		    border-collapse:collapse;
+            width: 600px;
+            border-collapse: collapse;
 		    }
 	    table td{
-	        text-align:center;
-		    border:#145b7d 1px solid
+            text-align: center;
+            border: #145b7d 1px solid
 		    }
 	    </style>
 	    </head>
         <body>
         <table>
         '''
-
         for i in range(1,m+1):
             html += '<tr>'
             for j in range(1, i+1):
                 html += '<td>%d*%d=%d</d>'%(i,j,i*j)
-            html += '</tr>'
-        
+            html += '</tr>'  
         html += '''
         </table>
         </body>
@@ -71,8 +41,7 @@ class SecondHandler(tornado.web.RequestHandler):
         self.write(html)
         
 application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r"/([0-9])", SecondHandler),
+    (r"/([0-9])?", MainHandler),
 ])
 
 if __name__ == "__main__":
